82a142f29fdb097ccb82bb8959671d51bbc1da93
spacewar <singhritik2711@gmail.com>
2022-01-13 13:16:54 +0530

    double linked list deletion operation

--- lab/dll.c
+++ lab/dll.c
@@ -46,7 +46,6 @@ struct node * insertAtTail(struct node *head,int val)
     return head;
 };
 
-
 struct node *nodeList(struct node *head)
 {
     int val,n;
@@ -81,9 +80,62 @@ void display(struct node *head)
     printf("NULL");
 }
 
+struct node *insertbefore(struct node *head,int val,int key)
+{
+    struct node *newnode;
+    struct node *temp,*p;
+    newnode=(struct node *)malloc(sizeof(struct node));
+    temp=head;
+    while(temp->next->data!=key)
+    {
+        temp=temp->next;
+    }
+    p=temp;
+    newnode->data=val;
+    newnode->next=temp->next;
+    newnode->prev=temp;
+    temp->next=newnode;
+
+    return head;
+
+};
+
+int middle_position(struct node *head)
+{
+    int count = 0;
+    if(head != NULL)
+    {
+        count++;
+    }
+
+    struct node *temp=head;
+    while(temp->next!=NULL)
+    {
+        count++;
+        temp=temp->next;
+    }
+    return count/2;
+}
+
+void deleteMiddle(struct node *head)
+{
+    struct node *temp=head;
+    struct node *p;
+    int i=0;
+    int index;
+    index=middle_position(head);
+    while(i!=index)
+    {
+        temp=temp->next;
+        i++;
+    }
+    temp->prev->next=temp->next;
+    temp->next->prev=temp->prev;
+}
+
 int main()
 {
-    int choice,val;
+    int choice,val,key;
     struct node *head=(struct node *)malloc(sizeof(struct node));
     while(1)
     {
@@ -91,7 +143,8 @@ int main()
         printf("\n2:Display");
         printf("\n3:Add to empty list");
         printf("\n4:Add to head");
-
+        printf("\n5:Insert before");
+        printf("\n6:Delete middle");
         printf("\nEnter your choice: ");
         scanf("%d",&choice);
 
@@ -113,6 +166,17 @@ int main()
             scanf("%d",&val);
             head=insertAtHead(head,val);
             break;
+        case 5:
+            printf("\nEnter key: ");
+            scanf("%d",&key);
+            printf("\nEnter value:");
+            scanf("%d",&val);
+            insertbefore(head,val,key);
+            break;
+        case 6:
+            deleteMiddle(head);
+            break;
+
         }
     }
 
